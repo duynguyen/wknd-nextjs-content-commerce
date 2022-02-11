7cc5c6743542bba8e5041f9918a83918f79dd773
James Zetlen <jzetlen@adobe.com>
2022-02-11 12:21:34 -0600

    feat: env-aware AdventureClient (#1)
    Some of the code didn't use the environment variables
    NEXT_PUBLIC_AEM_HOST and NEXT_GRAPHQL_ENDPOINT. Collected the adventure
    query methods into a client class with a static factory method that
    creates and reuses a single instance configured by the environment.
    
    Co-authored-by: Duy Nguyen <dunguyen@adobe.com>

--- pages/adventures/[id].js
+++ pages/adventures/[id].js
@@ -1,11 +1,10 @@
 import Layout from '../../components/layout'
-import { getAdventurePaths, getAdventureByPath } from '../../lib/adventures'
-import Head from 'next/head'
-import utilStyles from '../../styles/utils.module.css'
-
-const { NEXT_PUBLIC_AEM_HOST } = process.env
+import { AdventureClient } from '../../lib/adventures';
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Adventure({ adventure }) {
+  const { NEXT_PUBLIC_AEM_HOST } = process.env;
   return (
     <Layout adventure>
       <Head>
@@ -16,31 +15,46 @@ export default function Adventure({ adventure }) {
         <div className={utilStyles.lightText}>
           <p>{adventure.adventureType}</p>
         </div>
-        <img height="300px" alt={adventure.adventureTitle} src={`${NEXT_PUBLIC_AEM_HOST}${adventure.adventurePrimaryImage._path}`}/>
-        <div dangerouslySetInnerHTML={{ __html: adventure.adventureDescription.html }} />
+        <img
+          height="300px"
+          alt={adventure.adventureTitle}
+          src={`${NEXT_PUBLIC_AEM_HOST}${adventure.adventurePrimaryImage._path}`}
+        />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: adventure.adventureDescription.html,
+          }}
+        />
       </article>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const paths = await getAdventurePaths()
+  const client = AdventureClient.fromEnv();
+  const paths = await client.getAdventurePaths();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const folder = params.id
+  const client = AdventureClient.fromEnv();
+  const folder = params.id;
   // TODO: rev routing with AEM CF path
-  const cf = ('beervana-portland' === folder) ? 'beervana-in-portland' : (('surf-camp-in-costa-rica' === folder) ? 'surf-camp-costa-rica' : folder)
-  const path = `/content/dam/wknd/en/adventures/${folder}/${cf}`
-  const res = await getAdventureByPath(path)
-  const adventure = res?.data?.adventureByPath?.item
+  const cf =
+    'beervana-portland' === folder
+      ? 'beervana-in-portland'
+      : 'surf-camp-in-costa-rica' === folder
+      ? 'surf-camp-costa-rica'
+      : folder;
+  const path = `/content/dam/wknd/en/adventures/${folder}/${cf}`;
+  const res = await client.getAdventureByPath(path);
+  const adventure = res?.data?.adventureByPath?.item;
   return {
     props: {
-      adventure
-    }
-  }
+      adventure,
+    },
+  };
 }
--- pages/adventures/index.js
+++ pages/adventures/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
-import { getAdventures } from '../../lib/adventures'
-import ListItem from '../../components/ListItem'
-
-const { NEXT_PUBLIC_AEM_HOST } = process.env
+import { AdventureClient } from '../../lib/adventures';
+import ListItem from '../../components/ListItem';
 
 export default function Adventures({ adventures }) {
+  const { NEXT_PUBLIC_AEM_HOST } = process.env;
   return (
     <Layout>
       <Head>
@@ -15,30 +14,39 @@ export default function Adventures({ adventures }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h1 className={utilStyles.headingXl}>All adventures</h1>
         <ul className={utilStyles.list}>
-          {adventures.map(({ _path, adventureTitle, adventureDescription, adventurePrimaryImage }) => {
-            const pathItems = _path.split("/")
-            const path = `/adventures/${pathItems[pathItems.length - 2]}`
-            return (
-              <ListItem
-                key={_path}
-                path={path}
-                title={adventureTitle}
-                description={adventureDescription.plaintext}
-                imageSrc={`${NEXT_PUBLIC_AEM_HOST}${adventurePrimaryImage._path}`} />
-            )
-          })}
+          {adventures.map(
+            ({
+              _path,
+              adventureTitle,
+              adventureDescription,
+              adventurePrimaryImage,
+            }) => {
+              const pathItems = _path.split('/');
+              const path = `/adventures/${pathItems[pathItems.length - 2]}`;
+              return (
+                <ListItem
+                  key={_path}
+                  path={path}
+                  title={adventureTitle}
+                  description={adventureDescription.plaintext}
+                  imageSrc={`${NEXT_PUBLIC_AEM_HOST}${adventurePrimaryImage._path}`}
+                />
+              );
+            }
+          )}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const res = await getAdventures()
-  const adventures = res?.data?.adventureList?.items
+  const client = AdventureClient.fromEnv();
+  const res = await client.getAdventures();
+  const adventures = res?.data?.adventureList?.items;
   return {
     props: {
-      adventures
-    }
-  }
+      adventures,
+    },
+  };
 }
