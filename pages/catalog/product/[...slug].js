dfe3dd3e3c29537b275928b020e8a795159c0493
Markus Haack <mhaack@adobe.com>
2022-03-14 16:39:55 +0100

    Add GraphQL client for commerce

--- pages/catalog/product/[...slug].js
+++ pages/catalog/product/[...slug].js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import { gql } from "@apollo/client";
+import client from "../../../lib/CommerceGraphQLClient";
 
 const { NEXT_PUBLIC_AEM_PATH } = process.env;
 
-export default function ProductPage({ pagePath, slug }) {
+export default function ProductPage({ pagePath, slug, product }) {
 
     // TODO: render product page
 
@@ -13,7 +15,8 @@ export default function ProductPage({ pagePath, slug }) {
             </Head>
             <article>
                 {pagePath}<br/>
-                {slug}
+                {slug}<br />
+                SKU: {product.sku}
             </article>
         </>
     );
@@ -24,6 +27,34 @@ export async function getServerSideProps(context) {
     const slug = context.params.slug;
     const pagePath = `${NEXT_PUBLIC_AEM_PATH}/catalog/product`;
 
+    const { data } = await client.query({
+        query: gql`{
+            products(filter: {url_key: {eq: "${slug}"}}) {
+                items {
+                    name 
+                    sku   
+                    description {
+                        html
+                    }
+                    image {
+                        url
+                        label
+                    }
+                    price {
+                        regularPrice {
+                            amount {
+                                currency
+                                value
+                            }
+                        }
+                    }
+                }
+            }
+        }`
+    });
+    const product = data?.products?.items[0];
+
+    console.log(product)
 //  TODO: fetch from AEM and Commerce
 //    const [adobeCommerce, aem] = await Promise.all([
 //        fetch(/*adobe commerce*/),
@@ -33,6 +64,7 @@ export async function getServerSideProps(context) {
     return {
         props: {
             pagePath,
+            product: product,
             slug: slug.join('/'),
         }
     }
