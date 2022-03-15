9efc075de1700184b05dd0a820e1531186b84ac6
Markus Haack <mhaack@adobe.com>
2022-03-15 13:57:50 +0100

    Move to CommerceProductDetail

--- pages/catalog/product/[...slug].js
+++ pages/catalog/product/[...slug].js
@@ -1,20 +1,12 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { gql } from '@apollo/client';
 import client from '../../../lib/CommerceGraphQLClient';
-import usePrice from '../../../lib/use-price';
+import CommerceProductDetail from '../../../components/CommerceProductDetail';
 import styles from '../../../styles/Product.module.css';
 
 const { NEXT_PUBLIC_AEM_PATH } = process.env;
 
 export default function ProductPage({ pagePath, slug, product }) {
-
-    const price = usePrice({
-        amount: product.price.regularPrice.amount.value,
-        currencyCode: product.price.regularPrice.amount.currency,
-        locale: 'en-US'
-    });
-
     return (
         <>
             <Head>
@@ -24,33 +16,7 @@ export default function ProductPage({ pagePath, slug, product }) {
                 <div className={styles.content}>
                     <p>AEM Content PlaceHolder</p>
                 </div>
-                <div className={styles.grid}>
-                    <div className={styles.name}>
-                        <h1>{product.name}</h1>
-                    </div>
-                    <div className={styles.image}>
-                        <Image
-                            src={product.image.url}
-                            alt={product.image.label}
-                            width="500"
-                            height="600"
-                        />
-                    </div>
-                    <div className={styles.details}>
-                        <p>
-                            Price: <span>{price}</span>
-                        </p>
-                        <p>
-                            Sku: <span>{product.sku}</span>
-                        </p>
-                    </div>
-                    <div
-                        className={styles.description}
-                        dangerouslySetInnerHTML={{
-                            __html: product.description.html
-                        }}
-                    />
-                </div>
+                <CommerceProductDetail product={product}/>
                 <div className={styles.content}>
                     <p>AEM Content PlaceHolder</p>
                 </div>
