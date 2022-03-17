f000e3f2c4ae1975e3283fb77f913b9c23e5e13d
Duy Nguyen <dunguyen@adobe.com>
2022-03-17 15:30:09 +0100

    add preview mode

--- pages/api/getNextProps.js
+++ pages/api/getNextProps.js
@@ -1,15 +1,22 @@
 const { DOMParser } = require('xmldom')
 const { NEXT_PUBLIC_URL} = process.env
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   let { path } = req.query
 
-  fetch(NEXT_PUBLIC_URL + (path || ''))
-    .then(t => t.text())
-    .then(t => {
-      const parser = new DOMParser()
-      const doc = parser.parseFromString(t, 'text/html')
-      const data = doc.getElementById('__NEXT_DATA__').textContent
-      res.status(200).json(data)
-    });
+  const previewRes = await fetch(`${NEXT_PUBLIC_URL}/api/preview?slug=${(path || '/')}`)
+  const cookies = previewRes.headers.get('set-cookie')
+  const cookieReq = cookies.split(',').map(cookie => cookie.split(';')[0]).join(';')
+
+  const pageRes = await fetch(`${NEXT_PUBLIC_URL}${(path || '/')}`, {
+    headers: {
+      'Cookie': cookieReq
+    }
+  })
+
+  const pageHtml = await pageRes.text()
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(pageHtml, 'text/html')
+  const data = doc.getElementById('__NEXT_DATA__').textContent
+  res.status(200).json(data)
 }
