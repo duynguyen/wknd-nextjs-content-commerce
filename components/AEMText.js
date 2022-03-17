35fce005a1f7e32c6bfbcc36a602d4dc6d73e613
Dirk Rudolph <drudolph@adobe.com>
2022-03-17 11:19:19 +0100

    add the wcm core components text markup

--- components/AEMText.js
+++ components/AEMText.js
@@ -5,7 +5,7 @@ const { NEXT_PUBLIC_AEM_SITE } = process.env;
 
 export const TextEditConfig = {
     emptyLabel: 'Text',
-    isEmpty: function(props) {
+    isEmpty: function (props) {
         return !props || !props.text || props.text.trim().length < 1;
     },
     resourceType: `${NEXT_PUBLIC_AEM_SITE}/components/text`
@@ -13,12 +13,12 @@ export const TextEditConfig = {
 
 export const Text = ({ cqPath, richText, text }) => {
     const richTextContent = () => (
-        <div className="aem_text"
+        <div className='cmp-text'
             id={cqPath.substr(cqPath.lastIndexOf('/') + 1)}
             data-rte-editelement
-            dangerouslySetInnerHTML={{__html: text}} />
+            dangerouslySetInnerHTML={{ __html: text }} />
     );
-    return richText ? richTextContent() : (<div className="aem_text">{text}</div>);
+    return richText ? richTextContent() : (<div className="cmp-text"><p className='cmp-text__paragraph'>{text}</p></div>);
 };
 
 export const AEMText = withMappable(Text, TextEditConfig);
