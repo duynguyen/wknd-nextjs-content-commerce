d34396c5696abb23a47b96f6bd8c1837feb7425b
Laurentiu Magureanu <magurean@adobe.com>
2022-03-18 16:44:29 +0200

    Improve http functions

--- pages/api/removeItemFromCart.js
+++ pages/api/removeItemFromCart.js
@@ -1,6 +1,5 @@
 import { removeFromCart } from "../../lib/cartOperations";
 export default async function handler(req, res) {
-    const itemUid = req.query.itemUid;
-    const result = await removeFromCart(req, res, itemUid);
+    const result = await removeFromCart(req, res);
     res.status(200).json(result);
 }
